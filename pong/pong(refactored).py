801c95f07f4103b3ec6772017c5015d649784e4e
angel-cursed <cece06pesle@gmail.com>
2023-12-02 19:01:30 +0100

    ajout 2eme joueur

--- pong/pong(refactored).py
+++ pong/pong(refactored).py
@@ -11,15 +11,17 @@ class Pong():
         self.player = pygame.Rect(screen_widht - 20, screen_height/2 - 70, 10, 140)
         self.opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
+        #Game Mode
+        self.game_mode = None
+
         # Colors
         self.bg_color = (41, 163, 41)
-        # pygame.Color("grey12")
         self.yellow = (255, 255, 0)
+
         #Games Variables
         self.ball_speed_x = 7 * random.choice((1,-1))
         self.ball_speed_y = 7 * random.choice((1,-1))
         self.player_speed = 0
-        self.opponent_speed = 7
 
         #Text Variables
         self.player_score = 0
@@ -79,6 +81,14 @@ class Pong():
         if self.player.bottom >= screen_height:
             self.player.bottom = screen_height
 
+    def opponent_animation(self):
+
+        self.opponent.y += self.opponent_speed
+        if self.opponent.top <= 0:
+            self.opponent.top = 0
+        if self.opponent.bottom >= screen_height:
+            self.opponent.bottom = screen_height
+
     def opponent_ai(self):
 
         if self.opponent.top < self.ball.y:
@@ -114,6 +124,73 @@ class Pong():
             self.ball_speed_x = 7 * random.choice((1, -1))
             self.score_time = None
 
+    def set_mode(self,mode):
+        self.game_mode = mode
+        if mode == "solo":
+            self.opponent_speed = 7
+        else:
+            self.opponent_speed = 0
+
+class button():
+    def __init__(self, text, width, height, position: tuple, elevation):
+
+        #Core Attributes
+        self.pressed = False
+        self.elevation = elevation
+        self.dynamic_elevation = elevation
+        self.original_y = position[1]
+        self.text = text
+        self.click_time = None
+
+        #top rectangle
+        self.top_rect = pygame.Rect(position,(width, height))
+        self.top_color = game.bg_color
+
+        #bottom rectangle
+        self.bottom_rect = pygame.Rect(position,(width, height))
+        self.bottom_color = game.yellow
+
+        #text
+        self.text_color = (56,77,72)
+        self.text_surface = game.game_font.render(self.text, True, self.text_color)
+        self.text_rect = self.text_surface.get_rect(center = self.top_rect.center)
+
+    def draw(self):
+
+        #elevation logic
+        self.top_rect.y = self.original_y - self.dynamic_elevation
+        self.text_rect.center = self.top_rect.center
+
+        pygame.draw.rect(screen, self.bottom_color, self.bottom_rect, border_radius = 25)
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius = 25)
+        screen.blit(self.text_surface, self.text_rect)
+        self.check_click()
+
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(mouse_pos):
+            self.top_color = (146,220,229)
+            if pygame.mouse.get_pressed()[0]:
+                self.dynamic_elevation = 0
+                self.pressed = True
+            elif self.pressed:
+                self.dynamic_elevation = self.elevation
+                self.pressed = False
+                self.click_time = pygame.time.get_ticks()
+                self.clicked()
+        else:
+            self.dynamic_elevation = self.elevation
+            self.top_color = game.bg_color
+
+    def clicked(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.click_time >= 500:
+            if self.text == "Solo Mode":
+                game.set_mode("solo")
+            else:
+                game.set_mode("duo")
+
+
 
 #General Setup
 pygame.init()
@@ -125,22 +202,28 @@ screen_height = 760
 screen = pygame.display.set_mode((screen_widht, screen_height))
 pygame.display.set_caption("Pong")
 
-mode = None
+game = Pong(screen_widht, screen_height)
 
-while mode is None:
+button_solo = button("Solo Mode", 200, 100, (screen_widht/2 - 300, screen_height/2 - 50), 10)
+button_duo = button("Duo Mode", 200, 100, (screen_widht/2 + 100, screen_height/2 - 50), 10)
+
+while game.game_mode is None:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    if button_solo.click_time and not button_duo.click_time:
+        button_solo.clicked()
+    if button_duo.click_time and not button_solo.click_time:
+        button_duo.clicked()
         
 
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,255,255),(screen_widht/2 - 50,screen_height/2,50,50))
-    pygame.draw.rect(screen,(255,255,255),(screen_widht/2 + 50,screen_height/2,50,50))
+    screen.fill((136,160,168))
+    button_solo.draw()
+    button_duo.draw()
     pygame.display.update()
     clock.tick(60)
-
-game = Pong(screen_widht, screen_height)
 
 while True:
     #Handling Inputs
@@ -153,15 +236,32 @@ while True:
                 game.player_speed += 7
             if event.key == pygame.K_UP:
                 game.player_speed -= 7
+
+            if game.game_mode == "duo":
+                if event.key == pygame.K_z:
+                    game.opponent_speed -= 7
+                if event.key == pygame.K_s:
+                    game.opponent_speed += 7
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
                 game.player_speed -= 7
             if event.key == pygame.K_UP:
                 game.player_speed += 7
 
+            if game.game_mode == "duo":
+                if event.key == pygame.K_z:
+                    game.opponent_speed += 7
+                if event.key == pygame.K_s:
+                    game.opponent_speed -= 7
+
     game.ball_animation()
     game.player_animation()
-    game.opponent_ai()
+    if game.game_mode == "solo":
+        game.opponent_ai()
+    else:
+        game.opponent_animation()
+
     #Visuals
     screen.fill(game.bg_color)
     pygame.draw.rect(screen,game.yellow,game.player)
